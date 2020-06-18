e172eca10baebe0eea46d08647023d315a8d8454
mansi1496 <mansishah1496@gmail.com>
2020-06-18 20:15:19 +0530

    second commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonControlPanelComponent } from './button-control-panel/button-control-panel.component';
+import { TodoComponent } from './todo/todo.component';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ButtonControlPanelComponent,
+    TodoComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ButtonsModule,
+    BrowserAnimationsModule,
+    InputsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger, transition, useAnimation, style, group, query, animate, stagger, animateChild } from '@angular/animations';
+import { slideInAnimation, slideOutAnimation, moveDown } from '../animation';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss'],
+  animations: [
+    trigger('todoAnimations', [
+      transition(':enter', [
+        group([
+          query('h1',[
+            useAnimation(moveDown)
+          ]),
+          query('input',[ 
+            useAnimation(moveDown)
+          ]),
+          query('@todoItem',
+            stagger(100, animateChild()))
+        ])
+      ])
+    ]),
+
+    trigger('todoItem', [
+      transition(':ent2er', [
+       useAnimation(slideInAnimation)
+      ]),
+      transition(':leave', [
+        style({ backgroundColor: '#df3920' }),
+        useAnimation(slideOutAnimation)
+      ]),
+    ]),
+  
+    trigger('todosWrapper', [
+      transition(':enter', [
+      group([
+        query('h1', [
+          style({ opacity: 0 }),
+          animate(300)
+        ]),
+        query('input', [
+          style({ opacity: 0 }),
+          animate(300)
+        ]),
+        query('@todoItem',
+          stagger(200, animateChild())
+        )
+      ])
+  
+      ])
+    ]),
+     
+    ]
+  })
+export class TodoComponent implements OnInit {
+
+  todos = [
+    {
+      item : 'Take dog to vet',
+    },
+    {
+      item : 'Take dog to vet',
+    },
+    {
+      item : 'Take dog to vet',
+    },
+    {
+      item : 'Take dog to vet',
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addTodo(input : HTMLInputElement){
+    this.todos = [{item : input.value}, ...this.todos];
+    input.value;
+  }
+  
+  removeTodo(i){
+    this.todos.splice(i,1)
+  }
+
+}
